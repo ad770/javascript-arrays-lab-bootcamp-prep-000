06cd57475505c133dc43d41c094ca963fe6a5ec5
arye dov eidelman <aryedov.e@gmail.com>
2018-02-01 16:18:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function prependKitten(name){
   return [name, ...kittens];
 }
 function removeLastKitten(){
-
+ return kittens.slice(-1);
 }
 function removeFirstKitten(){
  return kittens.slice(1);
