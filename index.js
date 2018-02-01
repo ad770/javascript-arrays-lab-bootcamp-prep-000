a245cb814272f4d4f99ee7d259cf4561dc410e89
arye dov eidelman <aryedov.e@gmail.com>
2018-02-01 16:03:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 const app = "I don't do much."
+
+Arrays
+
+kittens
+
+destructivelyAppendKitten(name)
+
+destructivelyPrependKitten(name)
+
+destructivelyRemoveLastKitten()
+
+destructivelyRemoveFirstKitten()
+
+appendKitten(name)
+
+prependKitten(name)
+
+removeLastKitten()
+
+removeFirstKitten()
